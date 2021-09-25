19060baa6b0d10055ed1b9118b2c4e908a6a67df
Aniruddha Das <aniruddha.das.852@gmail.com>
2021-09-26 03:07:16 +0530

    commit

---
+++ src/components/Details/Data.jsx
@@ -0,0 +1,29 @@
+// var AQIData = {
+//   data: [
+//     {
+//       image: "../rxns/one.png",
+//     },
+//     {
+//       image: "../../rxns/two.png",
+//     },
+//     {
+//       image: "../../rxns/three.png",
+//     },
+//     {
+//       image: "../../rxns/four.png",
+//     },
+//     {
+//       image: "../../rxns/five.png",
+//     },
+//   ],
+// };
+
+import AQI1 from "../../assets/rxns/one.png";
+import AQI2 from "../../assets/rxns/two.png";
+import AQI3 from "../../assets/rxns/three.png";
+import AQI4 from "../../assets/rxns/four.png";
+import AQI5 from "../../assets/rxns/five.png";
+
+var AQIData = [AQI1, AQI2, AQI3, AQI4, AQI5];
+
+export default AQIData;
