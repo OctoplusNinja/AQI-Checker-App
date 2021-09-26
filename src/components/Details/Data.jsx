586a49ee1439de36f0416ec3bac8f33f8c0c4323
lostgirljourney <falgunisarkar526@gmail.com>
2021-09-26 18:22:44 +0530

    commit

--- src/components/Details/Data.jsx
+++ src/components/Details/Data.jsx
@@ -1,23 +1,3 @@
-// var AQIData = {
-//   data: [
-//     {
-//       image: "../rxns/one.png",
-//     },
-//     {
-//       image: "../../rxns/two.png",
-//     },
-//     {
-//       image: "../../rxns/three.png",
-//     },
-//     {
-//       image: "../../rxns/four.png",
-//     },
-//     {
-//       image: "../../rxns/five.png",
-//     },
-//   ],
-// };
-
 import AQI1 from "../../assets/rxns/one.png";
 import AQI2 from "../../assets/rxns/two.png";
 import AQI3 from "../../assets/rxns/three.png";
@@ -34,24 +14,43 @@ import errImg from "../../assets/rxns/confused.png";
 import errGif from "../../assets/gifs/no-clue.gif";
 
 var AQIData = {
-  main: [
-    { image: AQI1, AQI: "Good", gif: GIF1, message: "Stay Home and stay safe" },
-    { image: AQI2, AQI: "Fair", gif: GIF2, message: "Stay Home and stay safe" },
-    {
-      image: AQI3,
-      AQI: "Moderate",
-      gif: GIF3,
-      message: "Stay Home and stay safe",
-    },
-    { image: AQI4, AQI: "Poor", gif: GIF4, message: "Stay Home and stay safe" },
-    {
-      image: AQI5,
-      AQI: "Very Poor",
-      gif: GIF5,
-      message: "Stay Home and stay safe",
-    },
-  ],
-  err: { image: errImg, gif: errGif },
+	main: [
+		{
+			image: AQI1,
+			AQI: "Good",
+			gif: GIF1,
+			message:
+				"Air is fair so be fair to your life and go for some outdoor activity.",
+		},
+		{
+			image: AQI2,
+			AQI: "Fair",
+			gif: GIF2,
+			message:
+				"Walking is good for health so go outside and have a long walk!",
+		},
+		{
+			image: AQI3,
+			AQI: "Moderate",
+			gif: GIF3,
+			message:
+				"If, if it's not very important to go out, do some gardening and take care of the plants.",
+		},
+		{
+			image: AQI4,
+			AQI: "Poor",
+			gif: GIF4,
+			message:
+				"Mask can save you from pollutants present in the air, wear mask and stay away from respiratory disorders!",
+		},
+		{
+			image: AQI5,
+			AQI: "Very Poor",
+			gif: GIF5,
+			message: "Stay Home and stay safe...",
+		},
+	],
+	err: { image: errImg, gif: errGif },
 };
 
 export default AQIData;
