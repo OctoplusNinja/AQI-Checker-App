30f00910c78f895214bf7b779331be1aa99cf39b
lostgirljourney <falgunisarkar526@gmail.com>
2021-09-26 19:11:42 +0530

    commit

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -4,7 +4,7 @@ import "./Search.css";
 import Details from "../Details/Details";
 
 const BaseURL = "http://api.openweathermap.org/data/2.5";
-const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
+const API_KEY = "6186c89f2fffc2195b93eb0338c72f51";
 
 function Search() {
 	const [city, setCity] = useState("");
